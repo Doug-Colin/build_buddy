97ac7e615bd61be444222f96dbbaa7a14e137530
Douglas Colin Moore <dougcolin@gmail.com>
2023-04-29 18:48:00 -0400

    User functionality routes set up

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,27 @@
 //descr: Register new user
-//route: Posts /api/users
+//route: POST /api/users
 //access: Public
 const registerUser = (req, res) => {
     res.json({message: 'Register User'})
 }
 
+//descr: Authenticate a user
+//route: POST /api/users/login
+//access: Public
+const loginUser = (req, res) => {
+    res.json({message: 'Login User'})
+}
+
+
+//descr: Get user data
+//route: GET /api/users/current-user
+//access: Public
+const getCurrentUser = (req, res) => {
+    res.json({message: 'User data display'})
+}
+
 module.exports = { 
     registerUser,
+    loginUser,
+    getCurrentUser,
  }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,13 @@
 //three routes here: register/create user, login, and get user info
 const express = require ('express')
 const router = express.Router()
-const { registerUser } = require('../controllers/userController')
+const { registerUser, loginUser, getCurrentUser } = require('../controllers/userController')
 
 //router for post/create request to add a user
 router.post('/', registerUser)
+
+router.post('/login', loginUser)
+
+router.get('/current-user', getCurrentUser)
 
 module.exports = router
