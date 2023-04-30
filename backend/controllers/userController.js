ba4844ad05c5c14ee4b4bc836b54aa07864c736d
Douglas Colin Moore <dougcolin@gmail.com>
2023-04-30 18:48:00 -0400

    User registration functional

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,24 +1,66 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
+const User = require('../models/userModel')
+
 //descr: Register new user
-//route: POST /api/users
+//route/endpoint: POST /api/users
 //access: Public
-const registerUser = (req, res) => {
-    res.json({message: 'Register User'})
-}
+const registerUser = asyncHandler(async (req, res) => {  //mongoose and bcrypt both use async methods so make async
+    const { name, email, password } = req.body
+
+    if(!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please add all fields.')
+    }
+
+    //check if user exists
+    const userExists = await User.findOne({ email })
+
+    //if so, throw error
+    if(userExists) {
+        res.status(400)
+        throw new Error('User already exists.')
+    }
+
+    //hash password with bcrypt dependency (generate a salt to hash the pw by calling bcrypt method genSalt())
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+    
+    //create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+    })
+
+    //
+    if(user) {
+        res.status(201).json({
+            _id: user.id,
+            name: user.name,
+            email: user.email
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
+})
 
 //descr: Authenticate a user
 //route: POST /api/users/login
 //access: Public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.json({message: 'Login User'})
-}
+})
 
 
 //descr: Get user data
 //route: GET /api/users/current-user
 //access: Public
-const getCurrentUser = (req, res) => {
+const getCurrentUser = asyncHandler(async (req, res) => {
     res.json({message: 'User data display'})
-}
+})
 
 module.exports = { 
     registerUser,
