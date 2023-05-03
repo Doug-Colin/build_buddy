efe96823cc860afc230b8420ba9b830feb71552d
Doug-Colin <dougcolin@gmail.com>
2023-05-03 18:48:00 -0400

    Protected User Routes

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken')
 const asyncHandler = require('express-async-handler')
 const User = require('../models/userModel')
 
+//This middleware protects our user routes
 const protect = asyncHandler(async (req, res, next) => {
     let token //initialize token
 
