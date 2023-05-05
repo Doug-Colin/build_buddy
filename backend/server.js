751f81eccd158f981f60e9151fe2ff71cc3f3dc9
Douglas Colin Moore <dougcolin@gmail.com>
2023-05-05 18:48:00 -0400

    Server Running

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,10 @@
-console.log("hello from serverland")
+// console.log("hello from serverland")
+
+const express = require('express')
+const dotenv = require('dotenv').config()
+const port = process.env.port || 5050
+
+const app = express()
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
