3b4d0057a11c514b1ce77cbbec43568ae28a1a47
Douglas Colin Moore <dougcolin@gmail.com>
2023-06-15 18:48:00 -0400

    Cleaned up code for  update tasks functionality w/ modal panel

--- frontend/src/components/TaskItem.jsx
+++ frontend/src/components/TaskItem.jsx
@@ -26,61 +26,7 @@
 //     )
 // }
 
-
-// //-------------------------------------------------- Bing/GPT-4 -----------------------------
-// import React, { useState } from "react"
-// import { useDispatch } from 'react-redux' 
-// import { deleteTask, updateTask } from '../features/tasks/taskSlice'
-
-// export default function TaskItem({task}) {
-//     const [isEditing, setIsEditing] = useState(false)
-//     const [text, setText] = useState(task.text)
-//     const dispatch = useDispatch()
-
-//     const handleEdit = () => {
-//         setIsEditing(true)
-//     }
-
-//     const handleSave = () => {
-//         dispatch(updateTask({id: task._id, text}))
-//         setIsEditing(false)
-//     }
-
-//     const handleCancel = () => {
-//         setIsEditing(false)
-//         setText(task.text)
-//     }
-
-//     return (
-//         <div className="task">
-//             <div>{new Date(task.createdAt).toLocaleString('en-US')}</div>
-//             {isEditing ? (
-//                 <>
-//                     <input 
-//                         type='text'
-//                         name='text'
-//                         id='text'
-//                         value={text}
-//                         onChange={(e) => setText(e.target.value)}
-//                     />
-//                     <button onClick={handleSave}>Save</button>
-//                     <button onClick={handleCancel}>Cancel</button>
-//                 </>
-//             ) : (
-//                 <>
-//                     <h2>{task.text}</h2>
-//                     <button onClick={handleEdit}>Edit</button>
-//                     <button onClick={() => dispatch(deleteTask(task._id))} className="close">
-//                         X
-//                     </button>
-//                 </>
-//             )}
-//         </div>
-//     )
-// }
-
-
-//-------------------------------------------------- Modal / Github -----------------------------
+//-------------------------------------------------- Update via Modal -----------------------------
 
 import { useDispatch } from 'react-redux';
 import { deleteTask } from '../features/tasks/taskSlice';
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -92,66 +92,7 @@
 //       )
 // }
 
-
-//-----------------------------------GPT-4 / Bing---------------------------------------------------
-// import React from 'react'
-// import { useEffect } from 'react'
-// import {useNavigate } from 'react-router-dom' 
-// import { useSelector, useDispatch } from 'react-redux' 
-// import TaskForm from '../components/TaskForm'
-// import TaskItem from '../components/TaskItem'
-// import Spinner  from '../components/Spinner'
-// import { getTasks } from '../features/tasks/taskSlice'
-// import { reset } from '../features/auth/authSlice'
-
-// export default function Dashboard() {
-//     const navigate = useNavigate()
-//     const dispatch = useDispatch()
-
-//     const {user} = useSelector((state) => state.auth)  
-//     const {tasks, isLoading, isError, message} = useSelector((state) => state.tasks)
-//     useEffect(() => {
-//         if(isError) {
-//             console.log(message);
-//         }
-//         if(!user) {
-//             navigate('/login')
-//         }
-        
-//         dispatch(getTasks())
-        
-//         return () => {
-//             dispatch(reset())
-//           }
-//     }, [user, navigate, isError, message, dispatch])
-
-//     return (
-//         <>
-//           <section className='heading'>
-//             <h1>Welcome {user && user.name}</h1>
-//             <p>Tasks Dashboard</p>
-//           </section>
-    
-//           <TaskForm />
-    
-//           <section className='content'>
-//             {isLoading ? (
-//                 <Spinner />
-//             ) : tasks.length > 0 ? (
-//               <div className='tasks'>
-//                 {tasks.map((task) => (
-//                   <TaskItem key={task._id} task={task} />
-//                 ))}
-//               </div>
-//             ) : (
-//               <h3>You have not set any tasks</h3>
-//             )}
-//           </section>
-//         </>
-//       )
-// }
-
-//---------------------------- Github  Modal ex.------------------------------------------------
+//---------------------------- Update tasks with Modal ------------------------------------------------
 
 import {useEffect} from 'react';
 import { Link, useNavigate } from 'react-router-dom';
