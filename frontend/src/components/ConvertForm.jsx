baab0896134cc36c9877e9db81e571fd66cc5298
Douglas Colin Moore <dougcolin@gmail.com>
2023-06-26 18:48:00 -0400

    Migrated state for unit conversions to Redux Slice and updated handleInputChange and handleCoversion to dispatch relevant actions; moved Convert.js rendering logic to ConvertForm.js, debugged resulting select box issues

--- frontend/src/components/ConvertForm.jsx
+++ frontend/src/components/ConvertForm.jsx
@@ -1,72 +1,93 @@
-import {useState} from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import convert from 'convert-units'
-//add function later to convertSlice for POST request to save converted value to note, and dispatch function here.
-//import {createConversionNote} from '../features/convert/convertSlice'
+import { setCurrentUnit, setNewUnit, setResult, setValue } from '../features/convert/convertSlice'
+
 
 export default function ConvertForm() {
-    //add the state for this form
-    const [convertValue, setConvertValue] = useState({
-        value: '',
-        currentUnit: '',
-        newUnit: '',
-        result: '',
-      })
+
+    const dispatch = useDispatch()
+
+    const value = useSelector((state) => state.convert.value)
+    const currentUnit = useSelector((state) => state.convert.currentUnit)
+    const newUnit = useSelector((state) => state.convert.newUnit)
+    const result = useSelector((state) => state.convert.result)
+    
+    
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+
+        if(name === 'value') {
+            dispatch(setValue(value))
+        }
+        else if (name === 'currentUnit') {
+            dispatch(setCurrentUnit(value))
+        }
+        else if (name === 'newUnit') {
+            dispatch(setNewUnit(value))
+        }
+    }
+    
+  
+
+    const handleConversion = () => {
+        dispatch(
+            setResult(convert(value).from(currentUnit).to(newUnit))
+        )
+    }
 
 
-    //fucntion to handle the input change of the value and 'unit type' select forms, saving them to state. 
-    const handleInputChange = (event) => {
-        const { name, value }= event.target;
-        setConvertValue((prevState) => ({
-          ...prevState,
-          [name]: value,
-        }))
-      }
-    
-      //function to handle the conversion by taking the state values and plugging them into a convert-units function 
-      const handleConversion = () => {
-        setConvertValue((prevState) => ({
-            ...prevState,
-            result: convert(prevState.value).from(prevState.currentUnit).to(prevState.newUnit)
-          }))
-      }
-    
+ 
     return (
         <div>
             <h3>Length Conversion</h3>
-            <section className='form'>
-            <form>
-                <div className='form-group'>
-                    <label htmlFor="value">Enter the value you would like to convert:</label>
-                    <input 
-                        type="number"
-                        name="value"
+            <section className='form-group'>
+            <form>   
+                    <label>
+                      Enter the value you would like to convert:
+                      <input
+                       type="number"
+                       name='value'
+                      value={value}
+                      onChange={handleInputChange} />
+                    </label>
+                    
+                    <label>
+                      Enter your current unit of measurement:
+                      <select
+                        name="currentUnit"
+                        value={currentUnit}
                         onChange={handleInputChange}
-                    />
-                </div>
-                <div className='form-group'>
-                    <label htmlFor="currentUnit">Select the unit you would like to convert from:</label>
-                    <select name="currentUnit" onChange={handleInputChange}>   
+                        >
                         {convert().possibilities("length").map((unit, index) => (
                             <option key={index} value={unit}>
                                 {unit}
                             </option>
                         ))}
-                    </select>
-                </div>
-                <div className='form-group'>
-                    <label htmlFor="newUnit">Select the unit you would like to convert from:</label>
-                    <select name="newUnit" onChange={handleInputChange}>   
-                        {convert().possibilities("length").map((unit, index) => (
+                      </select>
+                    </label>
+                    
+                    <label>
+                    Enter the unit of measurement you'd like to convert to:
+                      <select
+                      name="newUnit"
+                      value={newUnit}
+                      onChange={handleInputChange}
+                      >
+                      {convert().possibilities("length").map((unit, index) => (
                             <option key={index} value={unit}>
                                 {unit}
                             </option>
                         ))}
-                    </select>
-                </div>
-                <button onClick={handleConversion}>Convert</button>
-                <h2>`Your result is ${convertValue.result}</h2>
+                      </select>
+                    </label>
+                    
+
             </form>
-            </section>
+                <button className='btn' type='submit' onClick={handleConversion}>Convert</button>
+                <h2>{value} {currentUnit} equals {result} {newUnit}</h2>
+            
+            </section> 
         </div>
- )
+     )
 }
+
