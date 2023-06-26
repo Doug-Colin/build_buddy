1415156d1886a763c4089e3a02d2d57c55dee4a2
Douglas Colin Moore <dougcolin@gmail.com>
2023-06-26 18:48:00 -0400

    removed redundant code in ConverForm.jsx

--- frontend/src/components/ConvertForm.jsx
+++ frontend/src/components/ConvertForm.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import convert from 'convert-units'
 import { setValue, setCurrentUnit, setNewUnit, setResult,  } from '../features/convert/convertSlice'
